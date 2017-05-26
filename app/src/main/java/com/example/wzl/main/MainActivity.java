b57120ed1280484717e31a14e323a00a9bdaf0aa
changfeng <3001075786@qq.com>
2017-05-26 12:18:28 +0800

    添加水平方向的滚动

--- app/src/main/java/com/example/wzl/main/MainActivity.java
+++ app/src/main/java/com/example/wzl/main/MainActivity.java
@@ -2,27 +2,85 @@ package com.example.wzl.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private List<String> textList;
-    private VerticalHorizontalTextView textView;
+    private VerticalHorizontalTextView textView,clickTextView;
+    private VerticalHorizontalTextView horizontalTextView;
+    private Button btn;
+    private boolean isVertical = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setData();
+        initView();
+        setListener();
+    }
+
+    private void initView() {
         textView = (VerticalHorizontalTextView) findViewById(R.id.vertical_text_view);
+        horizontalTextView = (VerticalHorizontalTextView) findViewById(R.id.horizontal_text_view);
+        clickTextView = (VerticalHorizontalTextView) findViewById(R.id.click_text_view);
+        btn = (Button) findViewById(R.id.btn);
+
+        clickTextView.setTextStillTime(5000);
+        clickTextView.setAnimTime(400,100);
+        clickTextView.setTextList(textList);
+        clickTextView.startScroll();
+
         textView.setTextStillTime(4000);
         textView.setAnimTime(400,100);
         textView.setTextList(textList);
         textView.startScroll();
+
+        horizontalTextView.setTextStillTime(4000);
+        horizontalTextView.setAnimTime(4000,1000);
+        horizontalTextView.setTextList(textList);
+        horizontalTextView.startScroll();
+    }
+
+    private void setListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isVertical){
+                    isVertical = false;
+                    clickTextView.setTextStillTime(5000);
+                    clickTextView.setAnimTime(5000,1000);
+                    clickTextView.setOrientation(VerticalHorizontalTextView.SCROLL_HORIZONTAL);
+                }else {
+                    isVertical = true;
+                    clickTextView.setTextStillTime(5000);
+                    clickTextView.setAnimTime(400,100);
+                    clickTextView.setOrientation(VerticalHorizontalTextView.SCROLL_VERTICAL);
+                }
+            }
+        });
+
+        clickTextView.setListener(new VerticalHorizontalTextView.OnItemClickListener() {
+            @Override
+            public void itemClick(int position) {
+                Toast.makeText(MainActivity.this,"item"+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+
         textView.setListener(new VerticalHorizontalTextView.OnItemClickListener() {
+            @Override
+            public void itemClick(int position) {
+                Toast.makeText(MainActivity.this,"item"+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        horizontalTextView.setListener(new VerticalHorizontalTextView.OnItemClickListener() {
             @Override
             public void itemClick(int position) {
                 Toast.makeText(MainActivity.this,"item"+position,Toast.LENGTH_SHORT).show();
@@ -42,6 +100,8 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         if (textView != null) {
             textView.startScroll();
+            horizontalTextView.startScroll();
+            clickTextView.startScroll();
         }
     }
 
@@ -50,6 +110,8 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         if (textView != null) {
             textView.stopScroll();
+            horizontalTextView.stopScroll();
+            clickTextView.stopScroll();
         }
     }
 
@@ -58,6 +120,9 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         if (textView != null) {
             textView.onDestroy();
+            horizontalTextView.onDestroy();
+            clickTextView.onDestroy();
         }
     }
+
 }
--- app/src/main/java/com/example/wzl/main/VerticalHorizontalTextView.java
+++ app/src/main/java/com/example/wzl/main/VerticalHorizontalTextView.java
@@ -5,6 +5,8 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.IntDef;
+import android.support.annotation.RestrictTo;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
@@ -16,8 +18,12 @@ import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.support.annotation.RestrictTo.Scope.LIBRARY_GROUP;
 
 /**
  * Des:可以实现文本的横向和竖向滚动
@@ -27,8 +33,17 @@ import java.util.List;
 
 public class VerticalHorizontalTextView extends TextSwitcher implements ViewSwitcher.ViewFactory {
 
-    public static final int ORIENTATION_VERTICAL = 0;
-    public static final int OIRENTATION_HORIZONTAL = 1;
+    /** @hide */
+    @RestrictTo(LIBRARY_GROUP)      //?????
+    @IntDef(flag=true, value={
+            SCROLL_VERTICAL,
+            SCROLL_HORIZONTAL
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ScrollOrientation {}
+
+    public static final int SCROLL_VERTICAL = 0;
+    public static final int SCROLL_HORIZONTAL = 1;
 
     public static final int FLAG_START_SCROLL = 0;
     public static final int FLAG_STOP_SCROLL = 1;
@@ -36,11 +51,19 @@ public class VerticalHorizontalTextView extends TextSwitcher implements ViewSwit
     /**
      * 滚动的方向
      */
-    private int orientation = ORIENTATION_VERTICAL;
+    private int orientation = SCROLL_VERTICAL;
     private int mTextColor = Color.BLACK;
     private float mTextSize = 16f;
     private int currentId = -1;
     private boolean isScrolling = false;
+    /**
+     *动画时长
+     */
+    private long mAnimTime;
+    /**
+     * 动画起止位置
+     */
+    private int mAnimTranslate;
     private Context mContext;
     /**
      * 控制文案轮播的handler
@@ -60,9 +83,12 @@ public class VerticalHorizontalTextView extends TextSwitcher implements ViewSwit
     public void setListener(OnItemClickListener listener) {
         this.listener = listener;
     }
-    //    public void setOrientation(int orientation) {
-//        this.orientation = orientation;
-//    }
+
+    public void setOrientation(@ScrollOrientation int orientation) {
+        this.orientation = orientation;
+        //重新
+        setAnimTime(mAnimTime,mAnimTranslate);
+    }
 
     public void setmTextColor(int mTextColor) {
         this.mTextColor = mTextColor;
@@ -89,6 +115,7 @@ public class VerticalHorizontalTextView extends TextSwitcher implements ViewSwit
         mTextColor = array.getColor(R.styleable.VerticalHorizontalTextView_textColor,Color.BLACK);
         mTextSize = array.getDimension(R.styleable.VerticalHorizontalTextView_textSize,16);
         mMaxLines = array.getInteger(R.styleable.VerticalHorizontalTextView_maxLines,1);
+        orientation = array.getInt(R.styleable.VerticalHorizontalTextView_scrollOrientation, 0);
         array.recycle();
     }
 
@@ -98,12 +125,14 @@ public class VerticalHorizontalTextView extends TextSwitcher implements ViewSwit
      * @param translate
      */
     public void setAnimTime(long time,int translate){
+        mAnimTranslate = translate;
+        mAnimTime = time;
         try {
             removeAllViewsInLayout();
             setFactory(this);
             Animation inAnim;
             Animation outAnim;
-            if (orientation == ORIENTATION_VERTICAL){
+            if (orientation == SCROLL_VERTICAL){
                 inAnim = new TranslateAnimation(0,0,translate,0);
                 outAnim = new TranslateAnimation(0,0,0,(-translate));
             }else {
